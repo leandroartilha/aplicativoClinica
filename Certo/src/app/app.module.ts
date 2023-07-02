07cb05d053b070a0dd7dd5e88dcb837ddbd8efb7
Leandro Artilha <leandro.artilha@gmail.com>
2023-07-01 23:48:05 -0300

    implementacao do firebase

--- Certo/src/app/app.module.ts
+++ Certo/src/app/app.module.ts
@@ -10,10 +10,22 @@ import { AgendasService } from './services/agendas.service';
 
 import { environment } from 'src/environments/environment';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+     IonicModule.forRoot(),
+      AppRoutingModule,
+      AngularFireAuthModule,
+      AngularFireStorageModule,
+      AngularFirestoreModule.enablePersistence(),
+      AngularFireModule.initializeApp(environment.firebaseConfig)
+    ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     AgendasService
   ],
